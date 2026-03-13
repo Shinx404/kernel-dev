e2403cf768d0295f132b241774c0db597fddf8f5
Redouane Moudjaoui <moudjored@gmail.com>
2026-03-13 14:56:17 +0100

    kitoa works, but va_list currently is facing some issues (can't figure out how to know the amount of va arguments.).

--- calls/syscalls.c
+++ calls/syscalls.c
@@ -1,6 +1,8 @@
 #include <stdarg.h>
 
 
+char res[32] = "";
+
 int kstrlen(char *buf){
 	int counter = 0;
 	char ch = buf[0];
@@ -11,31 +13,55 @@ int kstrlen(char *buf){
 	return counter;
 }
 
-int kitoa(int n){
-	char string[] = "";
+char * kitoa(int n, int base){
+	char string[32] = "";
 	int rem;
 	int i = 0;
 	int isNegative = 0;
 	if(n < 0){
 		isNegative = 1;
 	}
-	while(n != 0){
-		rem = n % 10;
-		n /= 10;
-		string[i] = rem;
-		i++;
+	if(base == 10){
+		while(n != 0){
+			rem = n % 10;
+			n /= 10;
+			string[i] = rem + '0';
+			i++;
+		}
+	}
+	else if(base == 16){
+		while(n != 0 || n =/= 'x'){
+                        rem = n % 16;
+                        n /= 16;
+                        string[i] = (rem-10) + 'a';
+                        i++;
+                }
+	
 	}
 
 	int len = kstrlen(string);
-	char res[len] = "";
-
-	if(isNegative == 1){
-		res[0] = '-';
-		len++;
+	
+	if(base == 10){
+		if(isNegative == 1){
+			res[0] = '-';
+			for(int j = 0; j < len; j++){
+                		res[j + 1] = string[len - j - 1]; 
+        		}
+		}
+		else{
+			for(int j = 0; j < len; j++){
+				res[j] = string[len - j - 1];
+			}
+		}
 	}
-	for(int j = 1; j < len; j++){
-		
+	else if(base == 16){
+		res[0] = '0'
+		res[1] = 'x'
+		for(int j = 0; j < len; j++){
+                                res[j + 2] = string[len - j - 1];
+                        }
 	}
+	return res;
 }
 
 void kprintf(char *str_literal,...){
@@ -45,18 +71,19 @@ void kprintf(char *str_literal,...){
 
 	for(int i = 0; i < kstrlen(str_literal);i++){
 		if(str_literal[i] == '%'){
-			va_start(vars);
+			va_start(vars, str_literal);
 			switch(str_literal[i + 1]) {
 				case('d'):
-					int num = va_arg(args, int);
-					char res[] = kitoa(num); 
+					int num = va_arg(vars, int);
+					char *res = kitoa(num);
+				        vga[vgap] = str[j];	
 					vga[vgap + 1] = 0x07;
 					break;
 				case('s'):
 					char str[] = va_arg(args, char *);
-					for(int j = 0; j < strlen(str); j++;){
-						vga[vgap] = str[j];
-						vga[vgap + 1] = 0x07;
+					for(int j = 0; j < kstrlen(str); j++){
+						vga[vgap + j] = str[j];
+						vga[vgap + 1 + j] = 0x07;
 					}
 					break;
 				case('c'):
