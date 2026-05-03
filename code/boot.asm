cdf43644726a534f8c6ac15728bc94d829b877ef
Redouane Moudjaoui <moudjored@gmail.com>
2026-05-03 19:18:40 +0200

    everything should be ready for testing

--- code/boot.asm
+++ code/boot.asm
@@ -2,14 +2,16 @@ EXTERN kernel_main
 BITS 16
 ORG 0x7c00
 
-%include "gdt.asm"
 
-CALL setGdt
-
-032 MOV eax, cr0
-032 OR eax, 1
-032 MOV cr0, eax
-JMP 0x08:protected_mode
+start:
+	%include "gdt.asm"
+	CALL setGdt
+	032 MOV eax, cr0
+	032 OR eax, 1
+	032 MOV cr0, eax
+	JMP 0x08:protected_mode
+	times 510 - ($ - $$) db 0
+	dw 0xAA55
 
 BITS 32
 
@@ -21,7 +23,4 @@ protected_mode:
 	MOV fs, ax	
 	MOV gs, ax
 	CALL kernel_main
-
-times 510 - ($ - $$) db 0
-dw 0xAA55
 
