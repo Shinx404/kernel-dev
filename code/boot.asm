7acb37a0f94ab7dc39010ed9c50204a4dfc3236c
Redouane Moudjaoui <moudjored@gmail.com>
2026-05-03 21:46:00 +0200

    troubleshooting since it wont show on the qemu terminal, replaced the setGdt with an inline for said troubleshooting

--- code/boot.asm
+++ code/boot.asm
@@ -1,10 +1,40 @@
+GLOBAL start
 EXTERN kernel_main
 BITS 16
-%include "gdt.asm"
+
 
 start:
-	
-	CALL setGdt
+	jmp boot
+
+gdt_start:
+    dq 0
+
+gdt_code:
+    dw 0xFFFF
+    dw 0
+    db 0
+    db 0x9A
+    db 0xCF
+    db 0
+
+gdt_data:
+    dw 0xFFFF
+    dw 0
+    db 0
+    db 0x92
+    db 0xCF
+    db 0
+gdt_end:
+
+gdtr:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+boot:
+	MOV sp, 0x7C00
+	XOR ax, ax
+	MOV ds, ax	
+	lgdt [gdtr]
 	o32 MOV eax, cr0
 	o32 OR eax, 1
 	o32 MOV cr0, eax
@@ -20,7 +50,9 @@ protected_mode:
 	MOV ds, ax
 	MOV es, ax
 	MOV ss, ax
+	MOV esp, 0x90000
 	MOV fs, ax	
 	MOV gs, ax
-	CALL kernel_main
+	MOV byte [0xB8000], 'A'
+	MOV byte [0xB8001], 0x07
 
--- code/kernel.c
+++ code/kernel.c
@@ -59,50 +59,60 @@ void kprintf(char *str_literal,...){
 	char *vga = (char *)0xB8000;
 	int vgap = 0;
 	va_list vars;
-
+	va_start(vars, str_literal);
 	for(int i = 0; i < kstrlen(str_literal);i++){
 		if(str_literal[i] == '%'){
-			va_start(vars, str_literal);
 			switch(str_literal[i + 1]) {
 				case('d'):
 					int num = va_arg(vars, int);
 					char *res = kitoa(num,10);
 				        for(int j = 0; j < kstrlen(res);j++){
-						vga[vgap + j] = res[j];	
-						vga[vgap + 1 + j] = 0x07;
+						vga[vgap + j * 2] = res[j];	
+						vga[vgap + 1 + j * 2] = 0x07;
 					}
-					break;
+					i++;
+					vgap += kstrlen(res) * 2;
+					continue;
 				case('s'):
 					char *str = va_arg(vars, char *);
 					for(int j = 0; j < kstrlen(str); j++){
-						vga[vgap + j] = str[j];
-						vga[vgap + 1 + j] = 0x07;
+						vga[vgap + j * 2] = str[j];
+						vga[vgap + 1 + j * 2] = 0x07;
 					}
-					break;
+					i++;
+					vgap += kstrlen(str) * 2;
+					continue;
 				case('c'):
 					vga[vgap] = (char)va_arg(vars, int);
 					vga[vgap + 1] = 0x07;
-					break;
+					i++;
+					vgap += 2;
+					continue;
 				case('x'):
 					int hex = va_arg(vars, int);
                                         char *resx = kitoa(hex,16);
                                         for(int j = 0; j < kstrlen(resx);j++){
-                                                vga[vgap + j] = resx[j];
-                                                vga[vgap + 1 + j] = 0x07;
+                                                vga[vgap + j * 2] = resx[j];
+                                                vga[vgap + 1 + j * 2] = 0x07;
                                         }
-                                        break;
+					i++;
+					vgap += kstrlen(resx) * 2;
+					continue;
 
 			}
+
+
 		}
 		else{
 			vga[vgap] = str_literal[i];
 			vga[vgap + 1] = 0x07;
+			vgap += 2;
 		}
-		vgap += 2;
 	}
 }
 
 int kernel_main(void){
 	kprintf("hello world %d", 123);
+	while(1);
 	return 0;
 }
